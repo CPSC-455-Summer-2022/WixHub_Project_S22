7fe1096a72f3f7ec0bde52656b331df0653c9b44
Josh Tillson <tillson@students.cs.ubc.ca>
2022-06-21 19:55:43 -0700

    added routes to be completed

--- youfly_server/app.js
+++ youfly_server/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var destinationsRouter = require('./routes/destinations');
+var questionsRouter = require('./routes/questions');
+var destinationMappingRouter = require('./routes/destinationMapping');
 
 var app = express();
 
@@ -16,5 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/destinations', destinationsRouter);
+app.use('/questions', questionsRouter);
+app.use('/destinationMapping', destinationMappingRouter);
 
 module.exports = app;
