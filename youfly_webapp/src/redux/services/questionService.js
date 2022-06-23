73b26a8bd2dd9b2f25ef3074dcf3352318b6fc0f
Josh Tillson <tillson@students.cs.ubc.ca>
2022-06-23 16:23:39 -0700

    added question Service

---
+++ youfly_webapp/src/redux/services/questionService.js
@@ -0,0 +1,45 @@
+
+// input for the recommendation generator is JSON body with params - 'question1', 'question2', ..., 'question8' and 'id'
+const recommendationGenerator = async (input) => {
+
+    const response = await fetch('http://localhost:3001/questions/recommendation', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(input),
+        mode: 'cors'
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+        const errorMsg = data?.message;
+        throw new Error(errorMsg)
+    }
+
+    return data;
+};
+
+const getQuestions = async () => {
+    const response = await fetch('http://localhost:3001/questions', {
+        method: 'GET',
+        mode: 'cors'
+    });
+    return response.json();
+};
+
+const getQuestion = async (id) => {
+    const response = await fetch('http://localhost:3001/questions/find?' + new URLSearchParams({
+        id: id
+    }), {
+        method: 'GET',
+        mode: 'cors'
+    });
+    return response.json();
+};
+
+export default {
+    recommendationGenerator,
+    getQuestions,
+    getQuestion
+}; 
