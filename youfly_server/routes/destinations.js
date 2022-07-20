00d786269d4d2f93fdb4e8ed82a3784d77df3c09
Josh Tillson <tillson@students.cs.ubc.ca>
2022-07-20 16:18:24 -0700

    cleaned up backend routes

--- youfly_server/routes/destinations.js
+++ youfly_server/routes/destinations.js
@@ -2,23 +2,6 @@ var express = require('express');
 var router = express.Router();
 const Destination = require("../models/destinations");
 
-let destinations = [
-    { "id": 1, "city": "Vancouver", "country": "Canada" },
-    { "id": 2, "city": "London", "country": "England" },
-    { "id": 3, "city": "Amsterdam", "country": "Netherlands" },
-    { "id": 4, "city": "Dubai", "country": "United Arab Emirates" },
-    { "id": 5, "city": "Beijing", "country": "China" },
-    { "id": 6, "city": "Bali", "country": "Indonesia" },
-    { "id": 7, "city": "Crete", "country": "Greece" },
-    { "id": 8, "city": "Cabo San Lucas", "country": "Mexico" },
-    { "id": 9, "city": "Paris", "country": "France" },
-    { "id": 10, "city": "Oslo", "country": "Norway" },
-    { "id": 11, "city": "Rome", "country": "Italy" },
-    { "id": 12, "city": "New York", "country": "United States" },
-    { "id": 13, "city": "Kyushi", "country": "Japan" },
-    { "id": 14, "city": "Singapore", "country": "Singapore" },
-];
-
 /* GET destinations. */
 router.get('/', function (req, res, next) {
     //res.send(destinations);
@@ -28,19 +11,6 @@ router.get('/', function (req, res, next) {
 
 });
 
-/* GET a single destination listing in JSON format. */
-router.get('/find', function (req, res, next) {
-    const foundDestination = destinations.find(destinations => destinations.id === req.query.id);
-    return res.send(foundDestination);
-    // const destinationId = req.query.id;
-    // Destination.findById(destinationId).then((result) => {
-    //     if (result) {
-    //         res.send(result);
-    //     } else {
-    //         res.status(404).send();
-    //     }
-    // });
-});
 //replace top one, put id in path get by uuid
 router.get("/:id", function (req, res, next) {
     const destinationId = req.params.id;
@@ -56,7 +26,7 @@ router.get("/:id", function (req, res, next) {
 //by destinationID
 router.get("/destinationID/:id", function (req, res, next) {
     const destinationId = req.params.id;
-    Destination.find({destinationId: destinationId}).then((result) => {
+    Destination.find({ destinationId: destinationId }).then((result) => {
         if (result) {
             res.send(result);
         } else {
--- youfly_server/routes/users.js
+++ youfly_server/routes/users.js
@@ -3,63 +3,14 @@ var router = express.Router();
 var { v4: uuid } = require('uuid');
 const User = require("../models/users");
 
-let users = [
-  {
-    "id": uuid(),
-    "f_name": "Josh",
-    "l_name": "Tillson",
-    "country": "Canada",
-    "destinations": ["1", "5"],
-    "question_responses": [1, 2, 3, 4, 1, 2, 3, 4]
-  },
-  {
-    "id": uuid(),
-    "f_name": "Ronin",
-    "l_name": "Cunningham",
-    "country": "Canada",
-    "destinations": ["2"],
-    "question_responses": [1, 1, 1, 1, 1, 1, 1, 1]
-  },
-  {
-    "id": uuid(),
-    "f_name": "Sherman",
-    "l_name": "Lam",
-    "country": "Canada",
-    "destinations": ["1", "2", "3"],
-    "question_responses": [1, 2, 1, 2, 1, 2, 1, 2]
-  },
-  {
-    "id": 123456,
-    "f_name": "Kevin",
-    "l_name": "Zhao",
-    "country": "Canada",
-    "destinations": ["1"],
-    "question_responses": [2, 3, 4, 4, 4, 3, 3, 2]
-  },
-];
-
 // GET users listing in JSON format.
 router.get('/', function (req, res, next) {
   User.find().then((result) => {
-      res.send(result);
+    res.send(result);
   });
   // res.send(users);
 });
 
-/*this one does not fit as industrial standard, should pass id by path parameter*/
-/* GET a single user listing in JSON format. */
-router.get('/find', function (req, res, next) {
-  const foundUser = users.find(user => user.id === req.query.id);
-  return res.send(foundUser);
-  // const userId = req.query.id;
-  // User.findById(userId).then((result) => {
-  //   if (result) {
-  //     res.send(result);
-  //   } else {
-  //     res.status(404).send();
-  //   }
-  // });
-});
 // new find user api by id in path parameter
 router.get('/:id', function (req, res, next) {
   const userId = req.params.id;
@@ -88,21 +39,6 @@ router.post('/', function (req, res, next) {
   });
 });
 
-/*this one does not fit as industrial standard, should pass id by path parameter*/
-/* Delete a single user listing in stored users list*/
-router.delete('/delete', function (req, res) {
-  let i = 0;
-  users.forEach(function (user) {
-    if (user.id == req.query.id) {
-      delete users[i];
-      users = users.filter(user => user);
-    }
-    i += 1;
-  });
-  return res
-    .status(202)
-    .send(users);
-});
 // new delete user api by id in path parameter
 router.delete('/:id', function (req, res) {
   const userId = req.params.id;
@@ -118,10 +54,6 @@ router.delete('/:id', function (req, res) {
 // orginal /deleteAll path are depreciate for mongo.
 /* Delete all users in stored user list*/
 router.delete('/', function (req, res) {
-  // users = [];
-  // return res
-  //   .status(203)
-  //   .send(users);
   User.deleteMany({}).then((result1) => {
     User.find().then((result2) => {
       res.status(203).send(result2);
@@ -131,27 +63,13 @@ router.delete('/', function (req, res) {
 
 
 router.put('/edit', function (req, res) {
-  // let editedUser = "";
-  // users.forEach(function (user) {
-  //   if (user.id == req.body.id) {
-  //     user.f_name = req.body.f_name;
-  //     user.l_name = req.body.l_name;
-  //     user.country = req.body.country;
-  //     user.destinations = req.body.destinations;
-  //     user.question_responses = req.body.question_responses;
-  //     editedUser = user;
-  //   }
-  // });
-  // return res
-  //   .status(204)
-  //   .send(users);
   const userId = req.body.id;
   const updatedInfo = {
-    "f_name" : req.body.f_name,
-    "l_name" : req.body.l_name,
-    "country" : req.body.country,
-    "destinations" : req.body.destinations,
-    "question_responses" : req.body.question_responses,
+    "f_name": req.body.f_name,
+    "l_name": req.body.l_name,
+    "country": req.body.country,
+    "destinations": req.body.destinations,
+    "question_responses": req.body.question_responses,
   };
   User.findByIdAndUpdate(userId, updatedInfo).then(() => {
     User.find().then((result) => {
