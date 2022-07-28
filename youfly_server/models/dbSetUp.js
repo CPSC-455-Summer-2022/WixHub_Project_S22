165aa2ff238903a0f63b8c869f51f08c51dfb55c
Weichong Zhao <wchongz@students.cs.ubc.ca>
2022-07-28 16:50:51 -0700

    move user constant from users.js to dbSetUp.js

--- youfly_server/models/dbSetUp.js
+++ youfly_server/models/dbSetUp.js
@@ -132,28 +132,36 @@ const initializeUsers = async function () {
             "l_name": "Tillson",
             "country": "Canada",
             "destinations": ["1", "5"],
-            "question_responses": [1, 2, 3, 4, 1, 2, 3, 4]
+            "question_responses": [1, 2, 3, 4, 1, 2, 3, 4],
+            "email": "josh@tillson.com",
+            "password": "1234password"
         },
         {
             "f_name": "Ronin",
             "l_name": "Cunningham",
             "country": "Canada",
             "destinations": ["2"],
-            "question_responses": [1, 1, 1, 1, 1, 1, 1, 1]
+            "question_responses": [1, 1, 1, 1, 1, 1, 1, 1],
+            "email": "ronin@cunningham.com",
+            "password": "1234password"
         },
         {
             "f_name": "Sherman",
             "l_name": "Lam",
             "country": "Canada",
             "destinations": ["1", "2", "3"],
-            "question_responses": [1, 2, 1, 2, 1, 2, 1, 2]
+            "question_responses": [1, 2, 1, 2, 1, 2, 1, 2],
+            "email": "sherman@lam.com",
+            "password": "1234password"
         },
         {
             "f_name": "Kevin",
             "l_name": "Zhao",
             "country": "Canada",
             "destinations": ["1"],
-            "question_responses": [2, 3, 4, 4, 4, 3, 3, 2]
+            "question_responses": [2, 3, 4, 4, 4, 3, 3, 2],
+            "email": "kevin@zhao.com",
+            "password": "1234password"
         },
     ]);
 };
