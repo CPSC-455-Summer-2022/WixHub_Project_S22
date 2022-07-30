afb0091bbe08e3621efe71dd1416e66ac4325992
Ronin Cunningham <ronincunningham1@gmail.com>
2022-07-30 16:25:19 -0400

    revert login

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -15,13 +15,14 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from "../CommonComponents/Copyright";
 import { AuthContext } from '../../context/auth';
 import { loginUserAsync } from '../../redux/thunks/userThunks';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const theme = createTheme();
 
 export default function SignInSide() {
   const dispatch = useDispatch();
   const context = React.useContext(AuthContext);
+  const users = useSelector(state => state.userReducer.list);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -29,19 +30,10 @@ export default function SignInSide() {
     const res = await dispatch(loginUserAsync({
       email: data.get('email'),
       password: data.get('password'),
-      // email: "josh@tillson.com",
-      // password: "1234password",
     }));
     const userData = res.payload;
     context.login(userData);
-
-    
-    // console.log(emailPass);
-    // console.log(response)
-    // console.log({
-    //   email: data.get('email'),
-    //   password: data.get('password'),
-    // });
+    users.push(userData);
   };
 
   return (
@@ -54,7 +46,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
