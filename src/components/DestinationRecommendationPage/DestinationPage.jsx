1a469b19e473bb36d9334c86067e254fc4498adf
Ronin Cunningham <ronincunningham1@gmail.com>
2022-08-06 14:44:56 -0400

    Hook up entire UserDashboard to redux. Made ActivityCard link open in new tab

--- src/components/DestinationRecommendationPage/DestinationPage.jsx
+++ src/components/DestinationRecommendationPage/DestinationPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ImageHeroUnit } from '../CommonComponents/ImageHeroUnit';
 import { useLocation } from "react-router-dom";
 import Typography from '../CommonComponents/Typography';
@@ -6,25 +6,35 @@ import { ActivityAlbum } from "./ActivityAlbum";
 
 export const DestinationPage = () => {
 	const location = useLocation()
-	const passedInState = location.state
+	const [destination, setDestination] = useState({
+		image: "",
+		city: "",
+		country: "",
+		description: "",
+		activityRecommendations: []
+	})
+
+	useEffect(() => {
+		setDestination(location.state.destination)
+	}, [location])
+	
 	
 	return (
 		<React.Fragment>
 			<ImageHeroUnit
 			// !!!TODO: Make this not overlap with Navbar when on smaller screens
-			// !!!TODO: Make sure to de-reference the correct properties after Kevin is done adding to db model
-				backgroundImage={passedInState.destination.image}
-				header={`${passedInState.destination.city}, ${passedInState.destination.country}`}
-				description={passedInState.destination.description}
+				backgroundImage={destination.image}
+				header={`${destination.city}, ${destination.country}`}
+				description={destination.description}
 				hasStartIcon
 				linkTo="/UserDashboardPage"
 				buttonDescription="Back to Dashboard"
 				smallText=""
 			/>
 			<Typography variant="h3" marked="center" align="center" component="h2" marginTop={5} marginBottom={5}>
-        		Activities in {passedInState.destination.city}
+        		Activities in {destination.city}
       		</Typography>
-      		<ActivityAlbum activities={passedInState.destination.activityRecommendations} />
+      		<ActivityAlbum activities={destination.activityRecommendations} />
 		</React.Fragment>
 	);
 }
