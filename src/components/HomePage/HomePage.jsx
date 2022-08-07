7d15983f9f4f6606592ebca2cff5600a4ec5e635
Ronin Cunningham <ronincunningham1@gmail.com>
2022-08-07 00:36:08 -0400

    Add redux and component functionality to delete cards

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -12,19 +12,19 @@ const homePageDestinations = [
     city: "Vancouver",
     country: "Canada",
     image: "https://source.unsplash.com/Yc9h5SJdEzI",
-    _id: "1"
+    destinationId: "1"
   },
   {
     city: "Tokyo",
     country: "Japan",
     image: "https://source.unsplash.com/4HG5hlhmZg8",
-    _id: "2"
+    destinationId: "2"
   },
   {
     city: "London",
     country: "England",
     image: "https://source.unsplash.com/iXqTqC-f6jI",
-    _id: "3"
+    destinationId: "3"
   }
 ];
 
