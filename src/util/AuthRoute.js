eae67b67b0226a04cb328ebc732a219ce1b87786
Ronin Cunningham <ronincunningham1@gmail.com>
2022-08-08 12:56:13 -0400

    Change guard logic a bit

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -2,13 +2,12 @@ import React, { useContext, useEffect } from 'react';
 import { Outlet, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { AuthContext } from '../context/auth';
-const NUMBER_OF_QUESTIONS = 8
 
 const AuthRoute = () => {
   const { user } = useContext(AuthContext);
   const userObject = useSelector(state => state.userReducer.currUser)
 
-    if (user && Object.keys(userObject.question_responses) !== NUMBER_OF_QUESTIONS){
+    if (user && !userObject.hasOwnProperty("question_responses")){
       return <Navigate to="/QuestionsStepperPage" />
     } else if (user) {
       return <Outlet />
